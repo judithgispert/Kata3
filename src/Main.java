bd852c38d431ac38e1500d02fe514424e2c147ad
judithgispert <jugispert@gmail.com>
2024-01-16 13:44:01 +0100

    Add scanner to introduce the year and the month

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int year = 2023;
-        int month = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write a year:");
+        int year = input.nextInt();
+        System.out.println("Write a month:");
+        int month = input.nextInt();
         System.out.println("Is there Friday 13th in this month? " + dayOfWeek(year, month));
     }
     public static boolean dayOfWeek(int year, int month){
